4218f13fafacbe424c6818dd3bad517d7be2cb89
akhil <akhilyerubandi143@gmail.com>
2024-02-26 22:06:53 +0530

    change

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from . import crm
-
+from . import  urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(crm.urls)),
+    path('',include('crm.urls')),
 ]
