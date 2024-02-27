33a4f341a56894f4b8c309db03e20b558d8f4f76
akhil <akhilyerubandi143@gmail.com>
2024-02-28 00:04:38 +0530

    changes

--- crm/urls.py
+++ crm/urls.py
@@ -4,4 +4,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('logout/',views.logout_user,name='logout'),
+    path('register/',views.register_user,name='register'),
+    path('record/<int:pk>',views.customer_record,name='record')
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,4 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
-    return render(request,'home.html')
+    record=Record.objects.all()
+    
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.success(request,"please enter the correct credentials")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{'record':record})
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+def register_user(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(request,username=username,password=password)
+            login(request,user)
+            return redirect('home')
+    else:
+        form=SignUpForm()
+        return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form})
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer=Record.objects.get(id=pk)
+        return render(request,'record.html',{'customer':customer})
+    else:
+        messages.success(request,"You must be login..")
+        return redirect('home')
